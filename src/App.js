6223542da1ecf5a14765d292c36d8203ea9240fc
Ramzi Hammami <rmizkk@gmail.com>
2021-11-17 11:29:13 +0100

    hassen5ashmoukbir

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Compone from './comp/Compone';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Compone />
+    </div>
+  );
+}
+
+export default App;
